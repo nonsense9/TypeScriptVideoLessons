1527cd99c5bf19f8e393a608017d0ecfe56ab8e5
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-01 21:13:51 +0300

    Add files via upload
    Interfaces

---
+++ Lesson9.ts
@@ -0,0 +1,41 @@
+// interface ILength {
+//   length: number;
+// }
+
+// function getLength(variable: ILength): void {
+//   console.log('getLength', variable.length);
+// }
+
+// getLength([1, 2, 3, 4, 5]);
+
+// const box = {
+//   name: "WFM",
+//   length: 20
+// };
+
+// getLength(box);
+// getLength([1, 2, 3]);
+
+interface IUser {
+  name: string;
+  age?: number;
+  logInfo(info: string): void;
+}
+
+interface IGetYear {
+  getYear(): number;
+}
+
+class User implements IUser, IGetYear {
+  name: string = "user";
+  job: string;
+  newAge: number;
+
+  logInfo(info: string): void {
+    console.log(info);
+  }
+
+  getYear(): number {
+    return 200;
+  }
+}
