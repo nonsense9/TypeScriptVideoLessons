2ba5177d767ca2d37d706a6901674e5536411017
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-01 15:51:54 +0300

    Add files via upload
    Special Types. Enum, never, null

---
+++ Lesson6.ts
@@ -0,0 +1,21 @@
+enum Job {
+  Frontend,
+  Backend = 50,
+  Designer
+}
+
+const job: Job = Job.Backend;
+console.log(job);
+
+const job2: Job = Job.Designer;
+console.log(job2);
+
+function throwNewError(err: string): never {
+  throw new Error(err);
+}
+
+let newVar;
+newVar = null;
+
+let myNumber: number | null = 20;
+myNumber = null;
