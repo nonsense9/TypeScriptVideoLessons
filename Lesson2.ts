f805c43be429f5e296ac8f8731b7585f74503cd9
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-01 13:54:11 +0300

    Add files via upload
    Arrays

---
+++ Lesson2.ts
@@ -0,0 +1,16 @@
+let numArray1: number[] = [1, 2 ,3];
+let numArray2: Array <number> = [1, 2, 3];
+
+let strArray1: string[] = ['1', '2', '3'];
+let strArray2: Array <string> = ['1', '2', '3'];
+
+let boolArray1: boolean[] = [true, false];
+let boolArray2: Array <boolean> = [true, false];
+
+//tuples
+let array: [number, number, string] = [1, 2, '3'];
+
+let array2: [boolean, string, number] = [true, 'str', 22];
+
+//Incorrect version below
+let array3: Array<number, string> = [1, 's'];
