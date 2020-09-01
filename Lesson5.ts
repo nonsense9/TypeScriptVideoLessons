831acdfb22d05110c88a758159809ab06657e16f
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-01 15:26:56 +0300

    Add files via upload
    Configuration and Compilation

---
+++ Lesson5.ts
@@ -0,0 +1,12 @@
+type User = {name: string, age: number};
+
+const user: User = {
+  name: "WFM",
+  age: 20
+};
+
+function logUser(user: User): void {
+  console.log(user.name + ' ' + user.age);
+}
+
+logUser(user);
