b5d12ce78168c81d8980b7242bba7d03f891d2cb
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-01 16:16:10 +0300

    Add files via upload
    Classes

---
+++ Lesson7.ts
@@ -0,0 +1,21 @@
+class User {
+  private isTeacher: boolean;
+  protected age: number = 30;
+
+  constructor(public name: string, public job: string) {
+  }
+
+  private getAge(): number {
+    return this.age;
+  }
+
+  public setTitle(title: boolean) {
+    console.log(this.isTeacher);
+    this.isTeacher = title;
+    console.log(this.isTeacher);
+    console.log(this.getAge()); 
+  }
+}
+
+const user = new User('WFM', 'Frontend');
+user.setTitle(false);
