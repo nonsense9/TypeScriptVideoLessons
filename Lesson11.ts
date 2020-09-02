45f2801f1e9e0f26c3b84710cc8fc8a9146abde3
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-02 12:36:46 +0300

    Add files via upload
    Decorators

---
+++ Lesson11.ts
@@ -0,0 +1,32 @@
+// function logger(constrFn: Function) {
+//   console.log(constrFn);
+// }
+
+// function shouldLog(flag: boolean): any {
+//   return flag ? logger : null;
+// }
+
+// @shouldLog(true)
+// class User {
+//   constructor(public name: string, public age: number) {
+//     console.log('I am new user');
+//   }
+// }
+// ==========================================================
+
+function addShowAbility(constructorFn: Function) {
+  constructorFn.prototype.showHtml = function() {
+    const pre = document.createElement('pre');
+    pre.innerHTML = JSON.stringify(this);
+    document.body.appendChild(pre);
+  }
+}
+
+@addShowAbility
+class User {
+  constructor(public name: string, public age: number, public job: string) {}
+}
+
+let user = new User ('WFM', 20, 'Frontend');
+console.log(user);
+(<any>user).showHtml();
